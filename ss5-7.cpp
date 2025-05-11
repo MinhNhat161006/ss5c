1835f4e24278ef3bee3a6c9be694eb0abbcc5753
MinhNhat161006 <n24dtcn060@student.ptithcm.edu.vn>
2025-05-11 15:56:34 +0700

    Update ss5-7.cpp

--- ss5-7.cpp
+++ ss5-7.cpp
@@ -1,33 +1,20 @@
 #include <stdio.h>
 
-int ucln(int a, int b) {
-    while (a != b) {
-        if (a > b) {
-            a -= b;  
-        } else {
-            b -= a;  
+int main() {
+    int a, b, min;
+
+    printf("Nhap so nguyen duong thu nhat: ");
+    scanf("%d", &a);
+    printf("Nhap so nguyen duong thu hai: ");
+    scanf("%d", &b);
+
+    min = (a < b) ? a : b;
+    for(int i = min; i >= 1; i--) {
+        if (a % i ==0 && b % i ==0) {
+            printf("Uoc chung lon nhat la: %d", i);
+            break;
         }
     }
-    return a; 
-}
-
-int main() {
-    int num1, num2;
-
-    do {
-        printf("Nhập số nguyên dương thứ nhất: ");
-        scanf("%d", &num1);
-        printf("Nhập số nguyên dương thứ hai: ");
-        scanf("%d", &num2);
-
-        if (num1 <= 0 || num2 <= 0) {
-            printf("Cả hai số phải là số nguyên dương. Vui lòng nhập lại!\n");
-        }
-    } while (num1 <= 0 || num2 <= 0);
-
-    int result = ucln(num1, num2);
-
-    printf("Ước chung lớn nhất của %d và %d là: %d\n", num1, num2, result);
-
     return 0;
+
 }
